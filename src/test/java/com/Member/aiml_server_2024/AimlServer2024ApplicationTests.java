461db81fae877056ca11b5fa44f34fadf9c0e87e
bonanah <jynah119@naver.com>
2024-04-24 16:58:54 +0900

    Initial commit

---
+++ src/test/java/com/Member/aiml_server_2024/AimlServer2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Member.aiml_server_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AimlServer2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
