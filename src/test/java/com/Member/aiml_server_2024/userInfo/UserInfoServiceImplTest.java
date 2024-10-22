796942d1652fc019780bf4da6635247c5a08fc34
Lazernes <dbsauddn1@gmail.com>
2024-10-22 18:04:49 +0900

    sejenkin돌리기 위한 수정ã

--- src/test/java/com/Member/aiml_server_2024/userInfo/UserInfoServiceImplTest.java
+++ src/test/java/com/Member/aiml_server_2024/userInfo/UserInfoServiceImplTest.java
@@ -1,21 +1,21 @@
-package com.Member.aiml_server_2024.userInfo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.concurrent.ExecutionException;
-
-@SpringBootTest
-class UserInfoServiceImplTest {
-
-    @Autowired
-    private UserInfoServiceImpl userInfoService;
-
-    @Test
-    public void getUserInfo() throws ExecutionException, InterruptedException {
-        UserInfo us = userInfoService.getUserInfo("가나다");
-        System.out.println(us.getName());
-        System.out.println(us.getPhoneNum());
-    }
-}
+//package com.Member.aiml_server_2024.userInfo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.util.concurrent.ExecutionException;
+//
+//@SpringBootTest
+//class UserInfoServiceImplTest {
+//
+//    @Autowired
+//    private UserInfoServiceImpl userInfoService;
+//
+//    @Test
+//    public void getUserInfo() throws ExecutionException, InterruptedException {
+//        UserInfo us = userInfoService.getUserInfo("가나다");
+//        System.out.println(us.getName());
+//        System.out.println(us.getPhoneNum());
+//    }
+//}
